3cb1ca8ae3b8ac8ceff14bc16ee805677a7ab3b3
Troy Raen <troy.raen@pitt.edu>
2019-11-13 15:36:21 -0500

    updates main.py for use on Korriban

--- PURs/main.py
+++ PURs/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
 from matplotlib import pyplot as plt
 
 import asassn_data as ad
@@ -7,15 +8,16 @@ import unsupervised as uns
 
 
 # load data
-ffeats = 'features.dat'
+ffeats = Path('/home/tjr63/Documents/PGB/TOM_prop_unsupclust/features.dat')
+fcsv = Path('/home/tjr63/Documents/PGB/TOM_prop_unsupclust/asassn-catalog.csv')
 cfeat = 'W3-W4' # restricts dffeats to rows where this column is not null
-dfcsv, dffeats = ad.load_dfs(ffeats=ffeats, cfeat=cfeat, consol=True)
+dfcsv, dffeats = ad.load_dfs(fcsv=fcsv, ffeats=ffeats, cfeat=cfeat, consol=True)
 
 
 # restrict dfcsv rows
 d = ad.get_hiprob(dfcsv) # class_probability > 0.99
 
-feats = uc.csv_feats_dict
+feats = ad.csv_feats_dict
 ft = feats['colors'] + feats['other'][0:2] + feats['mags'][0:3]
 
 d = d.dropna(axis=0, subset=ft) # Use only rows with all features
@@ -23,9 +25,8 @@ d, __ = ad.set_type_info(d) # recalc numinType
 
 numHi = 10000
 numLow = 50
-d = ad.get_hilow(dfeats, numHi=numHi, numLow=numLow)
+d = ad.get_hilow(d, numHi=numHi, numLow=numLow)
 
 
 # run isolation forest
-forest, predics = uns.do_isolationForest(d[ft])
-print('works')
+forest, predics = uns.do_isoForest(d[ft])
