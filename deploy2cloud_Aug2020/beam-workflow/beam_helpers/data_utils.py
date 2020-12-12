19ab0a4d94064225a5269501bb40402aae564b15
Troy Raen <troy.raen@pitt.edu>
2020-12-12 18:01:00 +0000

    syntax, organizational cleanup

--- deploy2cloud_Aug2020/beam-workflow/beam_helpers/data_utils.py
+++ deploy2cloud_Aug2020/beam-workflow/beam_helpers/data_utils.py
@@ -6,17 +6,27 @@ import fastavro as fa
 from apache_beam import DoFn
 
 
-class ExtractAlertData(DoFn):
+class ExtractAlertDict(DoFn):
     def process(self, msg):
+        from io import BytesIO
+        from fastavro import reader
 
         # Extract the alert data from msg -> dict
-        maxsize = 1500000
-        with SpooledTemporaryFile(max_size=maxsize, mode='w+b') as temp_file:
-            temp_file.write(msg.data)
-            temp_file.seek(0)
-            alertDicts = [r for r in fa.reader(temp_file)]
+        with BytesIO(msg) as fin:
+            # print(type(fin))
+            alertDicts = [r for r in reader(fin)]
 
-        candid = alertDicts[0]['candid']
-        logging.info(f'Extracted alert data dict for candid {candid}')
-
+        # candid = alertDicts[0]['candid']
+        # logging.info(f'Extracted alert data dict for candid {candid}')
+        # print(f'{alertDicts[0]}')
         return alertDicts
+
+class StripCutouts(DoFn):
+    # before stripping the cutouts, the upload to BQ failed with the following:
+    # UnicodeDecodeError: 'utf-8 [while running 'WriteToBigQuery/WriteToBigQuery/_StreamToBigQuery/StreamInsertRows/ParDo(BigQueryWriteFn)-ptransform-133664']' codec can't decode byte 0x8b in position 1: invalid start byte
+    # See Dataflow job ztf-alert-data-ps-extract-bq
+    # started on December 7, 2020 at 1:51:44 PM GMT-5
+    def process (self, alertDict):
+        cutouts = ['cutoutScience', 'cutoutTemplate', 'cutoutDifference']
+        alertStripped = {k:v for k, v in alertDict.items() if k not in cutouts}
+        return [alertStripped]
--- deploy2cloud_Aug2020/beam-workflow/ztf-beam.py
+++ deploy2cloud_Aug2020/beam-workflow/ztf-beam.py
@@ -4,10 +4,12 @@
 import logging
 import apache_beam as beam
 from apache_beam.io import BigQueryDisposition as bqdisp
-from apache_beam.io import ReadFromPubSub, Write, WriteToBigQuery
+from apache_beam.io import ReadFromPubSub, WriteToBigQuery
 from apache_beam.io.gcp.bigquery_tools import RetryStrategy
-
 from apache_beam import DoFn
+
+import beam_helpers.data_utils as dutil
+from beam_helpers.filters import is_extragalactic_transient
 
 
 # gcp resources
@@ -28,52 +30,32 @@ gcloud_options.staging_location = f'gs://{beam_bucket}/staging'
 gcloud_options.temp_location = f'gs://{beam_bucket}/temp'
 worker_options = options.view_as(beam.options.pipeline_options.WorkerOptions)
 worker_options.disk_size_gb = 50
-worker_options.max_num_workers = 50
+worker_options.max_num_workers = 5
 options.view_as(beam.options.pipeline_options.StandardOptions).runner = 'DataflowRunner'
 
-class ExtractAlertDict(DoFn):
-    def process(self, msg):
-        from io import BytesIO
-        from fastavro import reader
 
-        # Extract the alert data from msg -> dict
-        with BytesIO(msg) as fin:
-            # print(type(fin))
-            alertDicts = [r for r in reader(fin)]
-
-        # candid = alertDicts[0]['candid']
-        # logging.info(f'Extracted alert data dict for candid {candid}')
-        # print(f'{alertDicts[0]}')
-        return alertDicts
-
-class StripCutouts(DoFn):
-    # before stripping the cutouts, the upload to BQ failed with the following:
-    # UnicodeDecodeError: 'utf-8 [while running 'WriteToBigQuery/WriteToBigQuery/_StreamToBigQuery/StreamInsertRows/ParDo(BigQueryWriteFn)-ptransform-133664']' codec can't decode byte 0x8b in position 1: invalid start byte
-    # See Dataflow job ztf-alert-data-ps-extract-bq
-    # started on December 7, 2020 at 1:51:44 PM GMT-5
-    def process (self, alertDict):
-        cutouts = ['cutoutScience', 'cutoutTemplate', 'cutoutDifference']
-        alertStripped = {k:v for k, v in alertDict.items() if k not in cutouts}
-        return [alertStripped]
-
+config_writeBQFullPacket = {
+                            # 'schema': 'SCHEMA_AUTODETECT',
+                            'project': PROJECTID,
+                            'create_disposition': bqdisp.CREATE_NEVER,
+                            'write_disposition': bqdisp.WRITE_APPEND,
+                            'validate': False,
+                            'insert_retry_strategy': RetryStrategy.RETRY_NEVER,
+                            'batch_size': 5000,
+}
 
 with beam.Pipeline(options=options) as bp:
     # Read from PS and extract data as dicts
     PSin = (bp | 'ReadFromPubSub' >> ReadFromPubSub(topic=input_PS_topic))
-    alertDicts = (PSin | 'ExtractAlertDict' >> beam.ParDo(ExtractAlertDict()))
-    alertDictsSC = (alertDicts | 'StripCutouts' >> beam.ParDo(StripCutouts()))
+    alertDicts = (PSin | 'ExtractAlertDict' >> beam.ParDo(dutil.ExtractAlertDict()))
+    alertDictsSC = (alertDicts | 'StripCutouts' >> beam.ParDo(dutil.StripCutouts()))
 
     # Upload to BQ
     # BQ encoding error until cutouts were stripped. see StripCutouts() for more details
     # alerts with no history cannot currently be uploaded -> RETRY_NEVER
     # TODO: track deadletters, get them uploaded to bq
-    bqDeadletters = (alertDictsSC | 'WriteToBigQuery' >> Write(WriteToBigQuery(
-                            output_BQ_table,
-                            # schema='SCHEMA_AUTODETECT',
-                            project = PROJECTID,
-                            create_disposition = bqdisp.CREATE_NEVER,
-                            write_disposition = bqdisp.WRITE_APPEND,
-                            validate = False,
-                            insert_retry_strategy = RetryStrategy.RETRY_NEVER,
-                            batch_size = 5000,
-                            )))
+    bqDeadletters = (alertDictsSC | 'WriteToBigQuery' >> WriteToBigQuery(
+                            output_BQ_table, **config_writeBQFullPacket
+                            ))
+
+
